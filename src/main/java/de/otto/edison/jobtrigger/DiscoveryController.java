42992bafe88f80ac8605553f55ccf516b2ca517b
Guido Steinacker <guido.steinacker@otto.de>
2015-09-05 15:42:41 +0200

    Added JavaDoc + removed unused controller.

--- src/main/java/de/otto/edison/jobtrigger/DiscoveryController.java
+++ src/main/java/de/otto/edison/jobtrigger/DiscoveryController.java
@@ -15,6 +15,18 @@ import static java.time.Duration.ofDays;
 import static java.util.Arrays.asList;
 
 /**
+ * The DiscoveryController is responsible to handle the discovery page of the JobTrigger.
+ *
+ * Discovery is used to lookup job definitions from a server. These job definitions contain
+ * information about the jobs of the server. JobTrigger is using the definition to find out,
+ * how often jobs should be triggered.
+ *
+ * The URL of the server used for discovery is either the URL of a edison-microservice (or
+ * some other implementation that is providing job definitions in the same way as edison microservices do),
+ * or the URL of a supported discovery service like Marathon. Discovery services provide information (especially URLs)
+ * about other services. These information is then used to get the list of possible microservices, and then to
+ * ask all of these services for job definitions.
+ *
  * @author Guido Steinacker
  * @since 05.09.15
  */
@@ -22,7 +34,8 @@ import static java.util.Arrays.asList;
 public class DiscoveryController {
 
     @RequestMapping(value = "/discover", method = RequestMethod.POST)
-    public String startDiscovery(@RequestParam String discoveryUrl, HttpServletResponse response) {
+    public String startDiscovery(final @RequestParam String discoveryUrl,
+                                 final HttpServletResponse response) {
         if (discoveryUrl.isEmpty()) {
             Cookie cookie = new Cookie("jobtrigger-discovery-url", "");
             cookie.setMaxAge(0);
@@ -36,7 +49,7 @@ public class DiscoveryController {
     }
 
     @RequestMapping(value = "/discover", method = RequestMethod.GET)
-    public ModelAndView discover(HttpServletRequest request) {
+    public ModelAndView discover(final HttpServletRequest request) {
         final String discoveryUrl = getDiscoveryUrlFrom(request);
         if (!discoveryUrl.isEmpty()) {
             return new ModelAndView("discover") {{
