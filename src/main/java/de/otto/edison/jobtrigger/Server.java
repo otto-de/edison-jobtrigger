e767422c60da2174daeac6434a957f6f44561500
Jens Schaa <jens.schaa@otto.de>
2015-11-10 11:26:17 +0100

    Adds PropertySource to application context

--- src/main/java/de/otto/edison/jobtrigger/Server.java
+++ src/main/java/de/otto/edison/jobtrigger/Server.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 /*
 Extend the basePackages to de.otto.edison so SpringBoot is able to find
 the components configured by edison-microservice
 */
 @ComponentScan(basePackages = "de.otto.edison")
+@PropertySource("version.properties")
 @SpringBootApplication
 public class Server {
 
