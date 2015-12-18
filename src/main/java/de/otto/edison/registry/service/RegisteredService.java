2a456a57904d5c39b1c82506425f62dd739221b3
Hermann Vocke <hermann.vocke@gmail.com>
2015-12-18 10:32:19 +0100

    added Builder

--- src/main/java/de/otto/edison/registry/service/RegisteredService.java
+++ src/main/java/de/otto/edison/registry/service/RegisteredService.java
@@ -35,6 +35,20 @@ public final class RegisteredService {
         this.lastUpdated = Instant.now();
     }
 
+    private RegisteredService(Builder builder) {
+        service = builder.service;
+        href = builder.href;
+        description = builder.description;
+        expireAfter = builder.expireAfter;
+        environment = builder.environment;
+        groups = builder.groups;
+        lastUpdated = builder.lastUpdated;
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
     public String getService() {
         return service;
     }
@@ -63,4 +77,56 @@ public final class RegisteredService {
         return lastUpdated.plus(expireAfter).isBefore(Instant.now());
     }
 
+
+    public static final class Builder {
+        private String service;
+        private String href;
+        private String description;
+        private Duration expireAfter;
+        private String environment;
+        private List<String> groups;
+        private Instant lastUpdated;
+
+        private Builder() {
+        }
+
+        public Builder withService(String val) {
+            service = val;
+            return this;
+        }
+
+        public Builder withHref(String val) {
+            href = val;
+            return this;
+        }
+
+        public Builder withDescription(String val) {
+            description = val;
+            return this;
+        }
+
+        public Builder withExpireAfter(Duration val) {
+            expireAfter = val;
+            return this;
+        }
+
+        public Builder withEnvironment(String val) {
+            environment = val;
+            return this;
+        }
+
+        public Builder withGroups(List<String> val) {
+            groups = val;
+            return this;
+        }
+
+        public Builder withLastUpdated(Instant val) {
+            lastUpdated = val;
+            return this;
+        }
+
+        public RegisteredService build() {
+            return new RegisteredService(this);
+        }
+    }
 }
