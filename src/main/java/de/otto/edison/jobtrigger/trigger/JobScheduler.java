431cd8eed330fb193d5be8d1f81a323f0f65fb21
Peter Fouquet <peter.fouquet@ottogroup.com>
2016-09-29 12:16:03 +0200

    Update JobScheduler.java
    Restart only changed trigger

--- src/main/java/de/otto/edison/jobtrigger/trigger/JobScheduler.java
+++ src/main/java/de/otto/edison/jobtrigger/trigger/JobScheduler.java
@@ -1,10 +1,20 @@
 package de.otto.edison.jobtrigger.trigger;
 
+import static com.google.common.collect.ImmutableSet.copyOf;
+import static org.slf4j.LoggerFactory.getLogger;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ScheduledFuture;
+
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.google.common.collect.Sets;
+import com.google.common.collect.Sets.SetView;
 
 /**
  *
@@ -18,18 +28,32 @@ public class JobScheduler {
 
     @Autowired
     private ThreadPoolTaskScheduler scheduler;
+    
+    private Map<JobTrigger, ScheduledFuture> taskMap = new HashMap<JobTrigger, ScheduledFuture>();
+
+    private static final Logger LOG = getLogger(JobScheduler.class);
 
     public void updateTriggers(final List<JobTrigger> jobTriggers) {
-        stopAllTriggers();
-        scheduler.initialize();
-        for (final JobTrigger jobTrigger : jobTriggers) {
-            scheduler.schedule(
-                    jobTrigger.getRunnable(),
-                    jobTrigger.getTrigger());
-        }
+    	SetView<JobTrigger> deletedTasks = Sets.difference(taskMap.keySet(), copyOf(jobTriggers));
+    	SetView<JobTrigger> newTasks = Sets.difference(copyOf(jobTriggers), taskMap.keySet());
+    	deletedTasks.forEach(e->stopTrigger(e)); 
+    	newTasks.forEach(e->startTrigger(e));
     }
 
-    public void stopAllTriggers() {
-        scheduler.destroy();
-    }
+	private void startTrigger(JobTrigger jobTrigger) {
+        LOG.info("Start JobTrigger " + jobTrigger);
+		ScheduledFuture<?> task = scheduler.schedule(jobTrigger.getRunnable(), jobTrigger.getTrigger());
+		taskMap.put(jobTrigger, task);
+	}
+
+	private void stopTrigger(JobTrigger jobTrigger) {
+        LOG.info("Stop JobTrigger " + jobTrigger);
+		taskMap.get(jobTrigger).cancel(false);
+		taskMap.remove(jobTrigger);
+	}
+
+	public void stopAllTriggers() {
+		taskMap.values().stream().forEach(e->e.cancel(false));
+		taskMap.clear();		
+	}
 }
