02c83537e30e78a6b39039d309b60d84c2bf6734
Peter Fouquet <peter.fouquet@ottogroup.com>
2016-09-30 09:45:14 +0200

    Erweiterung JobTrigger Test

--- src/test/java/de/otto/edison/jobtrigger/trigger/JobSchedulerTest.java
+++ src/test/java/de/otto/edison/jobtrigger/trigger/JobSchedulerTest.java
@@ -13,6 +13,8 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 import static org.mockito.Mockito.*;
 
+import java.util.concurrent.ScheduledFuture;
+
 public class JobSchedulerTest {
 
     @Mock
@@ -29,12 +31,65 @@ public class JobSchedulerTest {
     @Test
     public void testShouldScheduleAllTriggers() {
         Runnable myRunnable = () -> {};
-        Trigger myTrigger = mock(Trigger.class);
-        JobDefinition definition = mock(JobDefinition.class);
-        JobTrigger parameter = new JobTrigger(definition, myTrigger, myRunnable);
+        Trigger myTrigger1 = mock(Trigger.class);
+        JobDefinition definition1 = mock(JobDefinition.class);
+        Trigger myTrigger2 = mock(Trigger.class);
+        JobDefinition definition2 = mock(JobDefinition.class);
+        JobTrigger trigger1 = new JobTrigger(definition1, myTrigger1, myRunnable);
+        JobTrigger trigger2 = new JobTrigger(definition2, myTrigger2, myRunnable);
 
-        testee.updateTriggers(ImmutableList.of(parameter, parameter));
+        testee.updateTriggers(ImmutableList.of(trigger1, trigger2));
 
-        verify(scheduler, times(1)).schedule(myRunnable, myTrigger);
+        verify(scheduler, times(1)).schedule(myRunnable, myTrigger1);
+        verify(scheduler, times(1)).schedule(myRunnable, myTrigger2);
     }
+    
+    @Test
+    public void testShouldAddTriggers() {
+        Runnable myRunnable = () -> {};
+        Trigger myTrigger1 = mock(Trigger.class);
+        JobDefinition definition1 = mock(JobDefinition.class);
+        Trigger myTrigger2 = mock(Trigger.class);
+        JobDefinition definition2 = mock(JobDefinition.class);
+        JobTrigger trigger1 = new JobTrigger(definition1, myTrigger1, myRunnable);
+        JobTrigger trigger2 = new JobTrigger(definition2, myTrigger2, myRunnable);
+
+        testee.updateTriggers(ImmutableList.of(trigger1));
+
+        verify(scheduler, times(1)).schedule(myRunnable, myTrigger1);
+        
+        testee.updateTriggers(ImmutableList.of(trigger1, trigger2));
+     
+        verify(scheduler, times(1)).schedule(myRunnable, myTrigger2);
+        verify(scheduler, times(1)).schedule(myRunnable, myTrigger2);
+        verifyNoMoreInteractions(scheduler);
+    }
+
+    @Test
+    public void testShouldRemoveTriggers() {
+        Runnable myRunnable = () -> {};
+        Trigger myTrigger1 = mock(Trigger.class);
+        JobDefinition definition1 = mock(JobDefinition.class);
+        Trigger myTrigger2 = mock(Trigger.class);
+        JobDefinition definition2 = mock(JobDefinition.class);
+        JobTrigger trigger1 = new JobTrigger(definition1, myTrigger1, myRunnable);
+        JobTrigger trigger2 = new JobTrigger(definition2, myTrigger2, myRunnable);
+
+        ScheduledFuture task1 = mock(ScheduledFuture.class);
+        ScheduledFuture task2 = mock(ScheduledFuture.class);
+        
+        when(scheduler.schedule(myRunnable, myTrigger1)).thenReturn(task1);
+        when(scheduler.schedule(myRunnable, myTrigger2)).thenReturn(task2);
+        testee.updateTriggers(ImmutableList.of(trigger1, trigger2));
+
+        verify(scheduler, times(1)).schedule(myRunnable, myTrigger1);
+        verify(scheduler, times(1)).schedule(myRunnable, myTrigger2);
+
+        testee.updateTriggers(ImmutableList.of(trigger2));
+        
+        verify(task1, times(1)).cancel(false);
+        verify(task2, never()).cancel(false);
+        verifyNoMoreInteractions(scheduler);
+    }
+
 }
